48469087fa3ba20800589fdd3bf0301de7114262
anita <test@gmail.com>
2017-02-02 17:22:52 +0530

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by LaNet on 2/2/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
